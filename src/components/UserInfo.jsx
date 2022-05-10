a1af29a149eba6fc3703e22281b952e0afaeebdf
Vipul Vaishnav <007vipulrank1@gmail.com>
2022-05-10 12:24:39 +0530

    TODO: 404bug fix and mobile responsive

--- src/components/UserInfo.jsx
+++ src/components/UserInfo.jsx
@@ -10,15 +10,15 @@ const UserInfo = ({ url }) => {
   const [error, setError] = useState(false);
 
   useEffect(() => {
+    console.log('re-rendered');
     fetch(url)
       .then((response) => {
-        if (response.ok) {
+        if (response.status === 200) {
           return response.json();
         }
 
         if (response.status === 404) {
           setError(true);
-          throw new Error(response.statusText);
         }
       })
       .then((data) => {
@@ -28,6 +28,16 @@ const UserInfo = ({ url }) => {
         console.log(error);
       });
   }, [url]);
+
+  const created_at_gen = () => {
+    const obj = new Date(user.created_at);
+    const monthArr = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const date = obj.getUTCDate();
+    const month = monthArr[obj.getUTCMonth()];
+    const year = obj.getUTCFullYear();
+    const dateString = date + ' ' + month + ' ' + year;
+    return dateString;
+  };
 
   if (error) {
     return (
@@ -42,7 +52,7 @@ const UserInfo = ({ url }) => {
       {/* header */}
       <div className="profile-header">
         <div className="profile-image">
-          <img src={user.avatar_url} alt="user avatar" style={{ width: '200px', height: 'auto' }} />
+          <img src={user.avatar_url} alt="user avatar" />
         </div>
         <div className="profile-info">
           <div className="profile-name">
@@ -50,7 +60,7 @@ const UserInfo = ({ url }) => {
             <h3 className="profile-username">@{user.login}</h3>
           </div>
           <div className="profile-date">
-            <h2 className="profile-date-text">{user.created_at}</h2>
+            <h3 className="profile-date-text">Joined {created_at_gen()}</h3>
           </div>
         </div>
       </div>
@@ -58,15 +68,15 @@ const UserInfo = ({ url }) => {
       <p className="profile-bio">{user.bio === null ? 'This Profile has no bio' : user.bio}</p>
       {/* stats */}
       <div className="profile-stats">
-        <div className="profile-repos">
+        <div className="profile-repos profile-stats-block">
           <h3 className="profile-stats-title">Repos</h3>
           <p className="profile-stats-value">{user.public_repos}</p>
         </div>
-        <div className="profile-followers">
+        <div className="profile-followers profile-stats-block">
           <h3 className="profile-stats-title">Followers</h3>
           <p className="profile-stats-value">{user.followers}</p>
         </div>
-        <div className="profile-followings">
+        <div className="profile-followings profile-stats-block">
           <h3 className="profile-stats-title">Followings</h3>
           <p className="profile-stats-value">{user.following}</p>
         </div>
@@ -75,28 +85,28 @@ const UserInfo = ({ url }) => {
       <div className="profile-others">
         <ul className="profile-others-list">
           <li className="profile-others-item profile-others-location">
-            <span className="profile-location-icon">
+            <span className="profile-item-icon">
               <img src={Location} alt="icon-location" />
             </span>
-            <span className="profile-location-text">{user.location}</span>
+            <span className="profile-item-text">{user.location || 'Not Available'}</span>
           </li>
           <li className="profile-others-item profile-others-twitter">
-            <span className="profile-twitter-icon">
+            <span className="profile-item-icon">
               <img src={Twitter} alt="icon-twitter" />
             </span>
-            <span className="profile-twitter-text">{user.twitter_username}</span>
+            <span className="profile-item-text">{user.twitter_username || 'Not Available'}</span>
           </li>
           <li className="profile-others-item profile-others-website">
-            <span className="profile-website-icon">
+            <span className="profile-item-icon">
               <img src={Website} alt="icon-website-link" />
             </span>
-            <span className="profile-website-text">{user.blog}</span>
+            <span className="profile-item-text">{user.blog || 'Not Available'}</span>
           </li>
           <li className="profile-others-item profile-others-company">
-            <span className="profile-company-icon">
+            <span className="profile-item-icon">
               <img src={Company} alt="icon-company" />
             </span>
-            <span className="profile-company-text">{user.company}</span>
+            <span className="profile-item-text">{user.company || 'Not Available'}</span>
           </li>
         </ul>
       </div>
